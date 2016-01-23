951df3ca14376642755e4a0458772d3898f62062
Tiago Vasconcelos <tisvasconcelos@gmail.com>
2016-01-22 22:04:13 -0200

    Vraptor 4.2.0-RC2 initial project

---
+++ src/main/java/com/cael/vraptor/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.cael.vraptor.controllers;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+
+@Controller
+public class HomeController {
+
+	@Get("/")
+	public void index(){
+		
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/jsp/home/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello Cael!</h2>
+</body>
+</html>
